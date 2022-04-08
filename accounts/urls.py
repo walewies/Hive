aee9b6431ff9ac577f7c03bcd470c191e84741a4
Wian Lewies <lewieswian@gmail.com>
2022-04-08 20:15:44 +0200

    Adds password change functionality.

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', views.SignUp.as_view(), name="signup"),
     path('<slug:slug>/profile', views.ProfileView.as_view(), name="profile"),
-    path('<slug:slug>/profile/edit', views.EditProfileView.as_view(), name="edit_profile")
+    path('<slug:slug>/profile/edit', views.EditProfileView.as_view(), name="edit_profile"),
+    path('<slug:slug>/change-password', views.ChangeUserPassword.as_view(), name="change_password"),
+    path('<slug:slug>/validate-password-change', views.ValidateUserPassword, name="validate_password_change"),
+    path('<slug:slug>/password-change-success', views.PasswordChangeSuccess.as_view(), name="password_change_success")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
 from django.utils.text import slugify
+from django.contrib.auth.hashers import check_password
+from django.shortcuts import render, redirect
+from .models import User
 from . import forms
 
 # Create your views here.
@@ -33,3 +36,35 @@ class EditProfileView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy("accounts:profile", kwargs={"slug": self.request.user.slug })
+
+class ChangeUserPassword(TemplateView):
+    template_name = "ChangeUserPassword.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user"] = self.request.user
+        return context
+
+def ValidateUserPassword(request, slug):
+    UserObj = User.objects.get(username=request.user.username)
+    print(request.POST.get("old_password"))
+    print(make_password(request.POST.get("old_password")))
+    print(UserObj.password)
+    if check_password(request.POST.get("old_password"), UserObj.password):
+        if request.POST.get("new_password1") == request.POST.get("new_password2"):
+            UserObj.set_password(request.POST.get("new_password1"))
+            UserObj.save()
+            return redirect("accounts:password_change_success", slug=request.user.slug)
+        else:
+            return render(request, "ChangeUserPassword.html", {"non_matching_passwords": True, "incorrect_password": False})
+    else:
+        return render(request, "ChangeUserPassword.html", {"incorrect_password": True, "non_matching_passwords": False})
+
+
+class PasswordChangeSuccess(TemplateView):
+    template_name = "PasswordChangeSuccess.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user"] = self.request.user
+        return context
