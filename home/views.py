88b66cef0064a27de6ed466f029dca3b176b713c
Wian Lewies <lewieswian@gmail.com>
2022-04-15 13:58:45 +0200

    Minor code refactoring and adds CSS so active tab is highlighted.

--- home/views.py
+++ home/views.py
@@ -13,37 +13,27 @@ class HomePageView(TemplateView):
         user = self.request.user.slug
         current_post = Post.objects.get(pk=int(post_pk))
         current_likes_list = current_post.likes.split(",")
+        order = ""
 
         if user in current_likes_list:
             current_likes_list.remove(user)
-            current_likes_string = ""
-            if len(current_likes_list) > 0:
-                current_likes_string += current_likes_list[0]
-                for i in range(1, len(current_likes_list)):
-                    current_likes_string += ","
-                    current_likes_string += current_likes_list[i]
-            current_likes_amount = len(current_likes_list)
-            Post.objects.filter(pk=int(post_pk)).update(likes=current_likes_string)
-            Post.objects.filter(pk=int(post_pk)).update(likes_amount=current_likes_amount-1)
-            return JsonResponse({
-                "order": "like",
-                "likes_amount":  current_likes_amount - 1 # -1 Accounts for empty string at index 0.
-            }, status=200)
+            order = "like"
         else:
             current_likes_list.append(user)
-            current_likes_string = ""
-            current_likes_string += current_likes_list[0]
-            for i in range(1, len(current_likes_list)):
-                current_likes_string += ","
-                current_likes_string += current_likes_list[i]
-            current_likes_amount = len(current_likes_list)
-            Post.objects.filter(pk=int(post_pk)).update(likes=current_likes_string)
-            Post.objects.filter(pk=int(post_pk)).update(likes_amount=current_likes_amount-1)
-            return JsonResponse({
-                "order": "unlike",
-                "element": request.POST.get("element"),
-                "likes_amount":  current_likes_amount - 1 # -1 Accounts for empty string at index 0.
-            }, status=200)
+            order = "unlike"
+
+        current_likes_string = ""
+        current_likes_string += current_likes_list[0]
+        for i in range(1, len(current_likes_list)):
+            current_likes_string += ","
+            current_likes_string += current_likes_list[i]
+        current_likes_amount = len(current_likes_list)
+        Post.objects.filter(pk=int(post_pk)).update(likes=current_likes_string)
+        Post.objects.filter(pk=int(post_pk)).update(likes_amount=current_likes_amount-1)
+        return JsonResponse({
+            "order": order,
+            "likes_amount":  current_likes_amount - 1 # -1 Accounts for empty string at index 0.
+        }, status=200)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
